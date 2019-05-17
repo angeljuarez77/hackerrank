2319ff898be25e6ac044c31d0f7f3898a9f7f629
Angel Juarez <angeljuarez729@gmail.com>
2019-05-17 02:52:53 -0400

    solved

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -10,29 +10,35 @@ const arr2 = [
   [10, 8, -12]
 ];
 
+const arr3 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+];
+
 function diagonalDifference(arr){
   function firstDirection(){
-    const firstNum = arr[0][0];
-    const middleNum = arr[1][1];
-    const lastNum = arr[arr.length - 1][arr[0].length - 1];
-    const sum = firstNum + middleNum + lastNum;
+    const axisMax = arr.length - 1;
+    let sum = 0;
+    for(let i = 0; i < axisMax + 1; i++){
+      sum += arr[i][i];
+    }
     return sum;
   }
+  const firstResult = firstDirection();
 
   function secondDirection(){
-    const firstNum = arr[0][arr[0].length - 1];
-    const middleNum = arr[1][1];
-    const lastNum = arr[arr.length - 1][0];
-    const sum = firstNum + middleNum + lastNum;
+    const axisMax = arr.length - 1;
+    let sum = 0;
+    for(let i = 0; i < axisMax + 1; i++){
+      sum += arr[axisMax - i][i];
+    }
     return sum;
   }
+  const secondResult = secondDirection();
 
-  const firstResult = firstDirection()
-  const secondResult = secondDirection()
   return Math.abs(firstResult - secondResult);
 }
 
-const first = diagonalDifference(arr1);
-const second = diagonalDifference(arr2);
-console.log(first);
-console.log(second);
+console.log(diagonalDifference(arr3));
