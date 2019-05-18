8231a099fa73251c221ba587fc07006a39b665a0
Angel Juarez <angeljuarez729@gmail.com>
2019-05-18 02:00:55 -0400

    Solved
    I had to use the .reduce method to sort my array.
    
    From MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
    
    "However, both spread (...) and apply will either fail or return the wrong result if the array has too many elements, because they try to pass the array elements as function parameters. See Using apply and built-in functions for more details. The reduce solution does not have this problem."

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -3,7 +3,10 @@ const second = [-12, 123, 12332152341, 1234121212, 12]
 const third = [1, 1, 1, 1, 1];
 
 function birthdayCakeCandles1(arr){
-  return console.log(arr.filter(number => number === Math.max(...arr)).length);
+  const max = arr.reduce(function(a, b) {
+    return Math.max(a, b);
+  });
+  return console.log(arr.filter(number => number === max).length);
 }
 
 function birthdayCakeCandles(ar) {
