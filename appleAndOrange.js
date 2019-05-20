56cb864cf5073df3381b625d83021e0d6ca3fe42
Angel Juarez <angeljuarez729@gmail.com>
2019-05-19 21:50:41 -0400

    changing double loop to single loop

--- appleAndOrange.js
+++ appleAndOrange.js
@@ -7,13 +7,13 @@ const orangeTree = 12;
 
 function appleAndOrange(s, t, a, b, apples, oranges){
 
-  function formHouseRange(s, t){
-    const arr = [];
-    for(let i = s; s < t + 1; s++){
-      arr.push(s);
-    };
-    return arr;
-  };
+  // function formHouseRange(s, t){
+  //   const arr = [];
+  //   for(let i = s; s < t + 1; s++){
+  //     arr.push(s);
+  //   };
+  //   return arr;
+  // };
 
   function calculateFruitPlacement(arr, treePlacement){
     const newArr = [];
@@ -24,23 +24,28 @@ function appleAndOrange(s, t, a, b, apples, oranges){
     return newArr;
   };
 
-  function areTheyInRange(houseRange, fruitsPlacement){
+  function areTheyInRange(startOfHouse, endOfHouse, fruitsPlacement){
     let fruitCount = 0;
-    for(let i = 0; i < houseRange.length; i++){
-      for(let j = 0; j < fruitsPlacement.length; j++){
-        if(houseRange[i] === fruitsPlacement[j]){
-          fruitCount++;
-        }
-      };
+    for(let i = 0; i < fruitsPlacement.length; i++){
+      if(fruitsPlacement[i] > startOfHouse && fruitsPlacement < endOfHouse){
+        fruitCount++;
+      }
     };
+  // for(let i = 0; i < houseRange.length; i++){
+  //   for(let j = 0; j < fruitsPlacement.length; j++){
+  //     if(houseRange[i] === fruitsPlacement[j]){
+  //       fruitCount++;
+  //     }
+  //   };
+  // };
     return fruitCount;
   };
 
-  const samsHouse = formHouseRange(s, t);
+  // const samsHouse = formHouseRange(s, t);
   const applePlacements = calculateFruitPlacement(apples, a);
   const orangePlacements = calculateFruitPlacement(oranges, b);
-  let applesOnHouse = areTheyInRange(samsHouse, applePlacements);
-  let orangesOnHouse = areTheyInRange(samsHouse, orangePlacements);
+  let applesOnHouse = areTheyInRange(s, t, applePlacements);
+  let orangesOnHouse = areTheyInRange(s, t, orangePlacements);
 
   return console.log(`${applesOnHouse} \n${orangesOnHouse}`);
 };
